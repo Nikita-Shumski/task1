fe879a22cade067e9f3cb7d3c887f0272c33a7ee
Nikita <nik199809@mail.ru>
2024-01-04 23:43:10 +0300

    что-то

---
+++ Desktop/task/task3/task5/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 1;
+int s = 2;
+int d = 5;
+int f = 69;
+int g = 11;
+
+int max = a;
+
+if (s > max)
+{
+    max = s;
+}
+if (d > max)
+{
+    max = d;
+}
+if (f > max)
+{
+    max = f;
+}
+if (g > max)
+{
+    max = g;
+}
+Console.WriteLine(max);
