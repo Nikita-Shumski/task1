686032172b8533b07dcbc6a611babd9d900333f3
Nikita <nik199809@mail.ru>
2024-01-04 19:02:46 +0300

    proga

--- Desktop/task/task1/Program.cs
+++ Desktop/task/task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+
+int a = 100;
+Console.WriteLine(a);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
